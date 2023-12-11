1a8e5ce9029177469ea45dee4b51f93a38f1f482
yishai ben zvi <yishaibz@gmail.com>
2023-12-11 12:33:48 +0200

    scgpt embeddig for all tcga

--- short_utils.py
+++ short_utils.py
@@ -1,5 +1,6 @@
 import os
 import socket
+import nbformat
 from pathlib import Path
 
 
@@ -23,3 +24,29 @@ def get_base_dir():
         #raise error
         raise ValueError(f"Hostname '{hostname}' not found in directory mapping")
 
+def export_marked_cells(notebook_path, output_path):
+    '''
+    Exports code cells marked with #export in notebook_path to output_path
+    Args:
+        notebook_path:
+        output_path:
+
+    Returns:
+
+    '''
+    with open(notebook_path, 'r', encoding='utf-8') as f:
+        nb = nbformat.read(f, as_version=4)
+
+    export_cells = []
+    for cell in nb.cells:
+        if cell.cell_type == 'code' and cell.source.startswith('#export'):
+            export_cells.append(cell.source)
+
+    with open(output_path, 'w', encoding='utf-8') as f:
+        f.write('\n'.join(export_cells))
+
+if __name__ == '__main__':
+    base_dir = get_base_dir()
+    nb_path = Path(base_dir / 'scgpt/j_scgpt_utils/cell_emb_plot.ipynb')
+    output_path = Path(Path(base_dir / 'scgpt/j_scgpt_utils/cell_emb_plot.txt'))
+    export_marked_cells(nb_path, output_path)
